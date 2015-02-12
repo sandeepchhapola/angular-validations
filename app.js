dcf5e7928b0ad9b048c045c988929504323ddbd1
sandeepchhapola <schhapola0001@gmail.com>
2015-02-12 13:55:18 +0530

    logic modified

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
-angular.module("myApp", ["ngRoute"])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'form.html',
-                controller: 'homeCtrl'
-            })
-            .when('/dashboard', {
-                templateUrl: 'dashboard.html',
-                controller: 'dashboardCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
-    .controller("homeCtrl", function ($scope, $window, homeService) {
-        $("form").on('submit', function () {
-            var user = {
+angular.module("myApp", [])
+
+    .controller("mainCtrl", function ($scope) {
+        $scope.isFormSubmitted = false;
+        jQuery("form").on('submit', function () {
+            $scope.user = {
                 userEmail: document.userForm.uEmail.value,
                 userName: document.userForm.uName.value,
                 userMobile: document.userForm.uMobile.value
             };
-            homeService.setUser(user);
-            $window.location.href = "#/dashboard";
+            $scope.isFormSubmitted = true;
+            document.userForm.uEmail.value = '';
+            document.userForm.uName.value = '';
+            document.userForm.uMobile.value = '';
+            $scope.userForm.$pristine = true;
+            $scope.$apply();
         });
-    })
-    .controller("dashboardCtrl", function ($scope, homeService) {
-        $scope.user= homeService.getUser();
-
-    })
-    .service("homeService", function () {
-        var user = {};
-        this.setUser = function (usr) {
-            user = usr;
-        };
-        this.getUser = function () {
-            return user;
-        };
     });
